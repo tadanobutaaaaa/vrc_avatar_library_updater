10f5ac2961a2628859cecd6a70086d55b5d0d46c
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-27 02:56:02 +0900

    アプリのアップデート画面を改善し、進行状況を表示するプログレスバーを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Text } from "@chakra-ui/react"
+import { Text, Box, Flex } from "@chakra-ui/react"
+import { ProgressBar, ProgressLabel, ProgressRoot } from "@/components/ui/progress"
 
 function App() {
     return (
-        <>
-        <Text>インストールしています</Text>
-        </>
+        <Box 
+            height="100vh"      // ビューポート全体の高さを設定
+            width="100vw"       // ビューポート全体の幅を設定
+            overflow="hidden"   // スクロールバーを非表示にする
+            display="flex" 
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <Text fontSize="sm">アップデートしています。しばらくお待ち下さい...</Text>
+            <ProgressRoot w="300px" value={null} mt="10px">
+                <ProgressLabel mb="2">
+                    Updating...
+                </ProgressLabel>
+                <ProgressBar />
+            </ProgressRoot>
+        </Box>
     )
 }
 
