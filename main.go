b52abcf814215610ee0beaff842ef3891b626352
tadanobutaaaaa <youyah1004@gmail.com>
2025-01-27 02:17:58 +0900

    Windows向けにカスタムテーマ設定を追加し、背景色の透明度を修正

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -23,9 +24,18 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 255},
 		OnStartup:        app.startup,
 		DisableResize:    true, // フルスクリーンボタンを無効化
+		Windows: &windows.Options{
+            CustomTheme: &windows.ThemeSettings{
+				LightModeTitleBar:  windows.RGB(255, 255, 255),
+                LightModeTitleText: windows.RGB(0, 0, 0),
+                LightModeBorder:    windows.RGB(255, 255, 255),
+            },
+			Theme: windows.Light,
+			BackdropType: windows.None,
+        },
 		Bind: []interface{}{
 			app,
 		},
